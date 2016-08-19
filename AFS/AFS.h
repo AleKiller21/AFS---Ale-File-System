ae965f7faf01795a41bb81d513f07f354c53b78e
Alejandro Ferrera <aleferrera@hotmail.com>
2016-08-19 15:22:21 -0600

    Define on-disk structures

--- AFS/AFS.h
+++ AFS/AFS.h
@@ -1,6 +1,37 @@
 #pragma once
 
+struct SuperBlock
+{
+	int totalBlocks;
+	int freeBlocks;
+	int usedBlocks;
+	int blockSize;
+	int totalSize;
+	int totalInodes;
+	int freeInodes;
+	int bitmapBlock;
+	int directoryBlock;
+	int inodeTableBlock;
+	char fileSystemName[16];
+	char partition;
+};
 
+struct Directory
+{
+	char name[50];
+	int inode;
+	bool available;
+};
+
+struct Inode
+{
+	bool available;
+	int size;
+	int dataBlocks;
+	//DateCreated
+	//DateLastModified
+	int blockPointer;
+};
 
 class AFS
 {
